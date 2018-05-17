40b1971f5eab11a84e0496dac4a2c55e995570d0
amshelhack3r <amshelhack3r@gmail.com>
2018-05-17 23:37:50 +0100

    added a switch function for the display type

--- delivery_report.php
+++ delivery_report.php
@@ -8,12 +8,32 @@ $get_super= filter_input(INPUT_GET, 'q');
 if (!$get_super) {
     die('SUPER GLOBAL GET NOT SET');
 }
-
+//decode the json string obtained
 $decoded_json = json_decode($get_super);
 $name = $decoded_json->name;
 $period = $decoded_json->period;
 
 $delivery = new Delivery();
 
-$delivery->show($name, $period);
+//do a switch statement to display data in a when the user selects a display type
+$display=$decoded_json->display;
+
+switch($display){
+    case 'raw':
+        $delivery->show($name, $period);
+        break;
+    case 'net':
+        $delivery->showNet($name, $period);
+        break;
+    case 'variance':
+        $delivery->showVariance($name, $period);
+        break;
+    case 'all':
+        $delivery->showAll($name, $period);
+        break;
+}
+
+
+
+
 
