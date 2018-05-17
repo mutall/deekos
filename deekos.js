53456470b4745bc9937804d238aa11fb2c7239df
amshelhack3r <amshelhack3r@gmail.com>
2018-05-17 23:36:59 +0100

    create a handler class with methods for activating listeners

--- deekos.js
+++ deekos.js
@@ -1,32 +1,91 @@
+window.onload = function () {
+    var handler = new Handlers();
+    handler.startListeners()
+
+}
 //create a class delivery 
-function Delivery(){
-this.name='';
-this.period='';
-
-    this.passName=function(aName){
-        this.name=aName;
-    };
-
-    this.getPeriod=function(aPeriod){
-        this.period=aPeriod;
-        
-        json=JSON.stringify({"name" : this.name, "period" : this.period});
-        this.show(json);
+function Delivery() {
+    //create getter and setter values for branch name, period and display type
+    branch = {
+        get name() {
+            return this._name
+        },
+        set name(val) {
+            this._name = val
+        }
     };
     
-    this.show=function(arg){
-        var ajax= new XMLHttpRequest();
-        ajax.onreadystatechange=function(){
-            if(this.readyState === 4 && this.status ===200){
-//                var a=this.responseText;
-                document.getElementById("content").innerHTML=this.responseText;
-            }
-        };
-        ajax.open("GET", "delivery_report.php?q="+arg);
-        ajax.send(); 
-       
+    period = {
+        get period() {
+            return this._period
+        },
+        set period(aPeriod) {
+            this._period = aPeriod
+        }
     };
+    
+    display = {
+        get display() {
+            return this._display
+        },
+        set display(aDisplay) {
+            this._display = aDisplay
+        }
+    };
+
+
+
+    //use this method to process the information 
+    this.process = function () {
+        //test if a user has selected all three options
+        if(!this.branch && !this.period && !this.display){
+            alert('You have to select atleast 1 option');
+        }
+        
+        var json = JSON.stringify({
+            "name": this.branch,
+            "period": this.period,
+            "display": this.display
+        });
+        
+        window.open("delivery_report.php?q="+json);
+    }
 }
 
+//create a class for all handlers and exception 
+//methods housed in here will be primarily used for event handling and exception cating
+function Handlers() {
+    //this is a method to assign click listeners when the page has loaded
+    this.startListeners = function () {
+        let client = document.getElementsByClassName('client');
+        let period = document.getElementsByClassName('period');
+        let display = document.getElementsByClassName('display');
+        let ok = document.getElementById('continue');
+        let cancel = document.getElementById('cancel');
+        
 
+        for (x in client) {
+            client[x].onclick = function () {
+               delivery.branch=this.textContent;
+            }
+        }
 
+        for (x in period) {
+            period[x].onclick = function () {
+                delivery.period=this.textContent;
+            }
+        }
+
+        for (x in display) {
+            display[x].onclick = function () {
+                delivery.display=this.id;
+            }
+        }
+        
+        ok.onclick=function(){
+            delivery.process()
+        }
+    };
+    
+    
+}
