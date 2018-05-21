8e12860237544f67e25ac8add712f420d0cca48c
amshelhack3r <amshelhack3r@gmail.com>
2018-05-22 00:21:47 +0100

    merge conflicts fixed

---
+++ deekos.js
@@ -0,0 +1,114 @@
+window.onload = function () {
+    var handler = new Handlers();
+    handler.startListeners()
+
+}
+//create a class delivery 
+function Delivery() {
+    //create getter and setter values for branch name, period and display type
+    branch = {
+        get name() {
+            return this._name
+        },
+        set name(val) {
+            this._name = val
+        }
+    };
+
+    period = {
+        get period() {
+            return this._period
+        },
+        set period(aPeriod) {
+            this._period = aPeriod
+        }
+    };
+
+    display = {
+        get display() {
+            return this._display
+        },
+        set display(aDisplay) {
+            this._display = aDisplay
+        }
+    };
+
+
+
+    //use this method to process the information 
+    this.process = function () {
+        //test if a user has selected all three options
+        if (!this.branch && !this.period && !this.display) {
+            alert('You have to select atleast 1 option');
+        } else {
+
+            var json = JSON.stringify({
+                "name": this.branch,
+                "period": this.period,
+                "display": this.display
+            });
+
+            window.open("delivery_report.php?q=" + json);
+        }
+
+        var json = JSON.stringify({
+            "name": this.branch,
+            "period": this.period,
+            "display": this.display
+        });
+
+        window.open("delivery_report.php?q=" + json);
+    }
+}
+
+//create a class for all handlers and exception 
+//methods housed in here will be primarily used for event handling and exception cating
+function Handlers() {
+    //this is a method to assign click listeners when the page has loaded
+    this.startListeners = function () {
+        let client = document.getElementsByClassName('client');
+        let period = document.getElementsByClassName('period');
+        let display = document.getElementsByClassName('display');
+        let ok = document.getElementById('continue');
+        let cancel = document.getElementById('cancel');
+
+        this_ = this;
+        for (x in client) {
+            client[x].onclick = function () {
+                delivery.branch = this.textContent;
+                this_.activeSelector("branch", this);
+            }
+        }
+
+        for (x in period) {
+            period[x].onclick = function () {
+                delivery.period = this.textContent;
+                this_.activeSelector("period", this);
+            }
+        }
+
+        for (x in display) {
+            display[x].onclick = function () {
+                delivery.display = this.id;
+                this_.activeSelector("content", this);
+            }
+        }
+
+        ok.onclick = function () {
+            delivery.process()
+        }
+    };
+    //write a method activeSelector that takes one parameter html id
+    //The first parameter is a html id. 
+    this.activeSelector = function (id_param, button) {
+        let container = document.getElementById(id_param);
+        let current = container.getElementsByClassName("active");
+        if (!current[0]) {
+            console.log(this.className);
+            button.className += " active";
+        } else {
+            current[0].className = current[0].className.replace(" active", "");
+            button.className += " active";
+        }
+    }
+}
---
+++ delivery_interface.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'crud.php';
+require_once 'deekos.php';
+
+$d = new Delivery();
+
+?>
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Daily Deliveries</title>
+        <link rel="stylesheet" href="style.css">
+        <script type="text/javascript" src="deekos.js"></script>
+        <script type="text/javascript" >
+            var delivery= new Delivery();
+        </script>
+        </head>
+
+    <body>
+        <header id="header" class="col-12">
+            <nav class="card-panel ">
+                <div class="button-group branch" id="branch">
+                <?php
+                $d->showClient();
+            ?>
+                    </div>
+            </nav>
+        </header>
+        <aside class="card-panel all-Periods">
+            <div class="button-group" id="period" >
+            <?php 
+                $d->showPeriod();
+                ?>
+            </div>
+        </aside>
+        <div class=" card-panel" id="content">
+            <button class='display button' id="raw">RAW TABLE</button>
+            <button class='display button' id="net">NET VALUES</button>
+            <button class='display button' id="variance">VARIANCES FOR THAT BRANCH</button>
+        </div>
+        <div class="card-panel">
+            <p>You selected $foo branch</p>
+            <p>For $bar period</p>
+            <p>Display as $foo</p>
+            <div class="button-group">
+                <button id="continue" class="primary    button">CONTINUE</button>
+                <button id="cancel" class="button" >CANCEL</button>
+            </div>
+        </div>
+    </body>
+
+    </html>
