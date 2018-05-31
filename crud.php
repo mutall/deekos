f7089622f7c9d0d69e7c07d1b02a70b9425078b2
amshelhack3r <amshelhack3r@gmail.com>
2018-05-31 11:45:38 +0300

    merged database php with crud class

--- crud.php
+++ crud.php
@@ -1,26 +1,18 @@
 
 <?php
-require 'database.php';
 //inherit the method from database to test for a connection
 
-class crud extends database {
-   public $db;
+class crud extends mysqli {
     //invoke the super constructor to check for a connection
     public function __construct() {
-        parent::__construct();
-        $this->db=new database();
-        if ($this->db->connect_error) {
-          die('Connect Error: '.$this->db->connect_error);
-        }
+        parent::__construct('localhost', 'root', '', 'mutall_deekos');
     }
-
-
     //function for getting data from database and displaying
 
-    public function getData($query) {
-        $result= $this->db->query($query);
+    public function getData($sql) {
+        $result= $this->query($sql);
         if(!$result){
-           die('Failed'. $this->db->error);
+           die('Failed '. $this->error);
         }
         //create an array of the queryied data. To be iterated when the function
         //used
@@ -30,33 +22,6 @@ class crud extends database {
         }
         return $rows;
 
-    }
-    //function to execute a query based on an sql statement
-    public function execute($query) {
-        $result = $this->db->query($query);
-
-        if ($result == false) {
-            die('cannot execute query'. $this->db->connect_error) ;
-        } else {
-            echo "<a href=" .$_SERVER["REQUEST_URI"] ."</a>";
-        }
-        return $result;
-    }
-
-
-    //function to delete data from a database
-
-    public function delete($id, $table){
-        $query="DELETE FROM $table WHERE id=$id";
-
-        $result= $this->query($query);
-
-        if($result==false){
-            die('Cannot delete record: '.$this->db->connect_error);
-        }
-        else{
-            echo 'deleted succefully';
-        }
     }
 
 }
