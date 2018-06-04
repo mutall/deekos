c3d28c7857fecbddff11e74adb5ddda94954983b
amshelhack3r <amshelhack3r@gmail.com>
2018-06-04 11:32:41 +0300

    easy tracking of sql statements

--- subQueries.sql
+++ subQueries.sql
@@ -1,38 +1,42 @@
--- Create a query for selecting the maximum date of a price item 
-select 
-    max(date) as curr_date
-from 
-    price
+-- Create a query for selecting the maximum date of a price item
+SELECT
+	max(date) as curr_date,
+	product.product
+FROM
+	price INNER JOIN
+	product ON product.product=price.product
 WHERE 
-    `date`<'2030-01-01'
+	price.`date`<'2030-01-01'
+GROUP BY product.product
 
 -- Create a query for selecting the price value for the maximum date
-select 
-    product.name, 
-    price.`date`, 
-    price.value
-from 
-    price INNER JOIN 
-    product on price.product=product.product inner join 
-    -- sub query
-    (
-    select 
-        max(date) as curr_date
-    from 
-        price
-    WHERE 
-        `date`<'2030-01-01'
-        ) as curr_price on curr_price.curr_date=price.`date`
-        
+SELECT
+	max_date.product,
+	price.value
+FROM 
+	price INNER JOIN 
+	(
+		SELECT
+			max(date) as curr_date,
+			product.product
+		FROM
+			price INNER JOIN
+			product ON product.product=price.product
+		WHERE 
+			price.`date`<'2030-01-01'
+		GROUP BY product.product)as max_date ON max_date.curr_date=price.`date`
+		AND max_date.product=price.product
+	
+		
 -- Create a query that combines the two upper ones to display a product of value and quantity 
 -- for each client for each period
 SELECT
 	client.client,
 	period.period,
-	quantity.value,
 	product.product,
-	curr_price.value,
-	quantity.value*curr_price.value as cost
+	quantity.value as quantity,
+	curr_price.value as price,
+	IF(delivery.`type`='DISPATCH',quantity.value*curr_price.value,(quantity.value*curr_price.value)*-1)AS cost
 FROM
 	delivery INNER JOIN
 	client ON client.client=delivery.client INNER JOIN
@@ -40,16 +44,74 @@ FROM
 	quantity ON quantity.delivery=delivery.delivery INNER JOIN
 	product ON product.product=quantity.product INNER JOIN
 	(
-	SELECT
-		product.product, 
-		price.`date`, 
-		price.value
-	FROM 
-		price INNER JOIN product on price.product=product.product INNER JOIN 
+		SELECT
+			max_date.product,
+			price.value
+		FROM 
+			price INNER JOIN 
+			(
+				SELECT
+					max(date) as curr_date,
+					product.product
+				FROM
+					price INNER JOIN
+					product ON product.product=price.product
+				WHERE 
+					price.`date`<'2030-01-01'
+				GROUP BY product.product)as max_date ON max_date.curr_date=price.`date`
+				AND max_date.product=price.product
+	) as curr_price ON product.product=curr_price.product
+	ORDER BY client, period
+	
+	SELECT 
+		cost.client,
+		cost.period,
+		SUM(cost.cost) as calculated,
+		'calculated' as `type`
+	FROM
 		(
-		SELECT 
-			max(date) as curr_date
+		SELECT
+			client.client,
+			period.period,
+			product.product,
+			quantity.value as quantity,
+			curr_price.value as price,
+		IF(
+			delivery.`type`='DISPATCH',quantity.value*curr_price.value,(quantity.value*curr_price.value)*-1
+			)AS cost
 		FROM
-			price
-		WHERE 
-			`date`<'2030-01-01') as max_date on max_date.curr_date=price.`date`) as curr_price
+			delivery INNER JOIN
+			client ON client.client=delivery.client INNER JOIN
+			period ON period.period=delivery.period INNER JOIN
+			quantity ON quantity.delivery=delivery.delivery INNER JOIN
+			product ON product.product=quantity.product INNER JOIN
+			(
+				SELECT
+					max_date.product,
+					price.value
+				FROM 
+					price INNER JOIN 
+					(
+						SELECT
+							max(date) as curr_date,
+							product.product
+						FROM
+							price INNER JOIN
+							product ON product.product=price.product
+						WHERE 
+							price.`date`<'2030-01-01'
+						GROUP BY product.product)as max_date ON max_date.curr_date=price.`date`
+						AND max_date.product=price.product
+			) as curr_price ON product.product=curr_price.product) as cost
+	GROUP BY client, period
+	
+SELECT 
+	client.client, 
+	period.period,
+	gross.ksh,
+	'gross' as type
+FROM
+	gross INNER JOIN 
+	client on gross.client=client.client INNER JOIN
+	period ON period.period=gross.period
+	
